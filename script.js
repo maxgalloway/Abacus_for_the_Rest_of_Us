37307cdcd328533fcf8af6502f5fef16708dd8ea
MGC <maxvgc@gmail.com>
2012-10-07 13:45:17 -0500

    making calculator oo

--- script.js
+++ script.js
@@ -1,79 +1,163 @@
-    var tally , operator , temp , isClear;
-    
-    setup();
+var calculator = {
+    tally : 0,
 
-    function setup(){
+    operator : '+',
 
+    temp : 0,
+
+    isClear : true,
+
+    setup : function(){
         var numbers = document.getElementsByClassName('number');
         var operators = document.getElementsByClassName('operator');
-        
+
         for (var i = 0; i < numbers.length; i++) {
-            numbers[i].onclick = function(j) { return function() { update(j); }; } (numbers[i].value);
+            
+            numbers[i].onclick = function(){calculator.update(this.value)};
         }
-        
-        for( var i = 0; i < operators.length; i++){
-            operators[i].onclick = function(j){ return function() { evaluate(j); }; } (operators[i].value);
+
+        for( var k = 0; k < operators.length; k++){
+            operators[k].onclick = function(){calculator.evaluate(this.value)};
         }
-        
-        document.getElementById('clear').onclick = reset;
-           
-        reset();
 
-    }
-
-
-    function update(lastDigit){
-        temp = (10 * temp) + parseInt(lastDigit);
-        output(temp);
-
-    }
-
+        document.getElementById('clear').onclick = function(){calculator.reset()};
+    },
     
+    output : function ( val ){
+        document.getElementById('out').innerHTML = val;
+    },
 
-    function evaluate(operate){
+    update : function(lastDigit){
+        this.temp = (10 * this.temp) + parseInt(lastDigit);
+        this.output(this.temp);
+    },
 
-        if( isClear ){
-            tally = temp;
-            isClear = false;
+    evaluate : function(operate){
+        if( this.isClear ){
+            this.tally = this.temp;
+            this.isClear = false;
 
         } else {
 
-            switch (operator){
+            switch (this.operator){
 
                 case '+':
-                    tally += temp;
+                    this.tally += this.temp;
                     break;
 
                 case '-':
-                    tally -= temp;
+                    this.tally -= this.temp;
                     break;
 
                 case 'x':
-                    tally *= temp;
+                    this.tally *= this.temp;
                     break;
 
                 case '/':
-                    tally /= temp;
+                    this.tally /= this.temp;
                     break;
 
             }
         }
 
-        output(tally);
-        temp = 0;
-        operator = operate;
+        this.output(this.tally);
+        this.temp = 0;
+        this.operator = operate;
+    },
+
+    reset : function(){
+        console.log(typeof this);
+               console.log(this.tally);
+        console.log(this.temp);
+        this.tally = 0;
+        this.operator = '+';
+        this.temp = 0;
+        this.isClear = true; 
+        console.log(this.tally);
+        console.log(this.temp);
+        calculator.output(0);
     }
-    
-    function reset(){
-        tally = 0;
-        operator = '+';
-        temp = 0;
-        isClear = true; 
-        output(0);
-    }
-    
-    function output( val ){
-        document.getElementById('out').innerHTML = val;
-    }
+
+};
+
+calculator.setup();
+
+//var tally , operator , temp , isClear;
+//    
+//    setup();
+//
+//    function setup(){
+//
+//        var numbers = document.getElementsByClassName('number');
+//        var operators = document.getElementsByClassName('operator');
+//        
+//        for (var i = 0; i < numbers.length; i++) {
+//            numbers[i].onclick = function(j) { return function() { update(j); }; } (numbers[i].value);
+//        }
+//        
+//        for( var i = 0; i < operators.length; i++){
+//            operators[i].onclick = function(j){ return function() { evaluate(j); }; } (operators[i].value);
+//        }
+//        
+//        document.getElementById('clear').onclick = reset;
+//           
+//        reset();
+//
+//    }
+//
+//
+//    function update(lastDigit){
+//        temp = (10 * temp) + parseInt(lastDigit);
+//        output(temp);
+//
+//    }
+//
+//    
+//
+//    function evaluate(operate){
+//
+//        if( isClear ){
+//            tally = temp;
+//            isClear = false;
+//
+//        } else {
+//
+//            switch (operator){
+//
+//                case '+':
+//                    tally += temp;
+//                    break;
+//
+//                case '-':
+//                    tally -= temp;
+//                    break;
+//
+//                case 'x':
+//                    tally *= temp;
+//                    break;
+//
+//                case '/':
+//                    tally /= temp;
+//                    break;
+//
+//            }
+//        }
+//
+//        output(tally);
+//        temp = 0;
+//        operator = operate;
+//    }
+//    
+//    function reset(){
+//        tally = 0;
+//        operator = '+';
+//        temp = 0;
+//        isClear = true; 
+//        output(0);
+//    }
+//    
+//    function output( val ){
+//        document.getElementById('out').innerHTML = val;
+//    }
 
                         
