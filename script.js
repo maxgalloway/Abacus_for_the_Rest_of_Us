c65422274aad4002cddfc8350b4094e66b6a8560
MaGiC <maxvgc@gmail.com>
2012-11-03 18:11:17 -0500

    js is now super awesome, and will not be improved any further, thank you.

--- script.js
+++ script.js
@@ -3,131 +3,149 @@
 
 var calculator = new function(){
     
-//  What immediately follows would be the constructor in class-oriented
-//  languages. (But in javascript, the whole class definition is in 
-//  the constructor.)
-    
-//  First, initialize the calculator's instance variables
-
-    var that;
-        
-    this.init = function(screen){
-        
-        that = {
-        
-            tally : 0, // the result of all operations since a clear
-
-            operator : '+', // the last operator pressed
-
-            temp : 0, // the number being entered by user
-
-            isClear : true, // a flag, indicated whether there has been output
-
-            display : screen, // a dom elem to write result
-
-//            The evaluate function performs a computation. 
-//            First, it performs the stored operation on temp and tally.
-//            Then it stores the given operation for the next evaluation.        
-
-            evaluate : function(operate){
-
-//                Special condition if there has not been output yet.
-
-                if( this.isClear ){
-
-                    this.tally = this.temp;
-                    this.isClear = false;
-
-                } else {
-
-    //              Perform the stored operation on tally and temp
-
-                    switch (this.operator){
-
-                        case '+':
-                            this.tally += this.temp;
-                            break;
-
-                        case '-':
-                            this.tally -= this.temp;
-                            break;
-
-                        case 'x':
-                            this.tally *= this.temp;
-                            break;
-
-                        case '/':
-                            this.tally /= this.temp;
-                            break;
-
-                    }
-                }
-
-                this.output(this.tally); // show the new tally
-                this.temp = 0; // reset temp
-                this.operator = operate; // get ready to perform the given operation
-            },
-
-    //      The update function will take string representing a single digit, and 
-    //      append it to what the user has entered so far, both on the screen and
-    //      internally.        
-
-            update : function(lastDigit){
-                this.temp = (10 * this.temp) + parseInt(lastDigit);
-                this.output(this.temp);
-            },
-
-
-    //      The output function sets the calculator's screen to the given value.
-    //      The value, val should be a number. 
-
-            output : function ( val ){
-
-                if( typeof val === 'number'){
-                    this.display.innerHTML = val;
-                }
-            },
-
-    //      The reset function clears the calculator's instance variables, and 
-    //      wipes the screen.
-
-            reset : function(){
-                this.tally = 0;
-                this.operator = '+';
-                this.temp = 0;
-                this.isClear = true; 
-                this.output(0);
-            }
-
-        }; // end that
-        
-        if( !document.getElementsByClassName ){
-            
-            document.getElementsByClassName(c) = function(c){
+//  that is an internal representation of the calculator.
+//  By defining it as a var inside the pseudoclass, it
+//  and its properties, and its methods are invisible 
+//  to the outside world.
                 
-            };
-        }
+    var that = {};
+    
+//  The calculator's init method takes only a dom node, screen.
+//  This screen will be accessed by the inner calculator, that,
+//  to display the numeric output.    
+    
+    this.init = function(screen){ 
         
-    }; // end init
+//      to ensure that methods requiring the display are not fired without
+//      it first being initialized, all of that's methods and properties
+//      are defined in this function (which requires screen as a parameter)
+
+        if( typeof screen === 'object'){
+        
+            that = {
+
+                tally : 0, // the result of all operations since a clear
+
+                operator : '+', // the last operator pressed
+
+                temp : 0, // the number being entered by user
+
+                isClear : true, // a flag, indicated whether there has been output
+
+                display : screen, // init needs to be called bef
+
+//              The evaluate function performs a computation. 
+//              First, it performs the stored operation on temp and tally.
+//              Then it stores the given operation for the next evaluation.        
+
+                evaluate : function(operate){
+
+//                  Special condition if there has not been output yet.
+
+                    if( this.isClear ){
+
+                        this.tally = this.temp;
+                        this.isClear = false;
+
+                    } else {
+
+//                  Perform the stored operation on tally and temp
+
+                        switch (this.operator){
+
+                            case '+':
+                                this.tally += this.temp;
+                                break;
+
+                            case '-':
+                                this.tally -= this.temp;
+                                break;
+
+                            case 'x':
+                                this.tally *= this.temp;
+                                break;
+
+                            case '/':
+                                this.tally /= this.temp;
+                                break;
+
+                        }
+                    }
+
+//                  In either case, update the display, and prepare for next input
+
+                    this.output(this.tally); // show the new tally
+                    this.temp = 0; // reset temp
+                    this.operator = operate; // get ready to perform the given operation
+                },
+
+//              The update function will take string representing a single digit, and 
+//              append it to what the user has entered so far, both on the screen and
+//              internally.        
+
+                update : function(lastDigit){
+                    this.temp = (10 * this.temp) + parseInt(lastDigit);
+                    this.output(this.temp);
+                },
 
 
-//  Second, run setup routine, in which page elements are bound to
-//  calculator methods. 
-//  We will start by binding the numbers' click
-//  event to the calculator's update method.
+//              The output function sets the calculator's screen to the given value.
+//              The value, val should be a number. 
 
+                output : function ( val ){
 
+                    if( typeof val === 'number'){
+                        this.display.innerHTML = val;
+                    }
+                },
+
+//              The reset function clears the calculator's instance variables, and 
+//              wipes the screen.
+
+                reset : function(){
+                    this.tally = 0;
+                    this.operator = '+';
+                    this.temp = 0;
+                    this.isClear = true; 
+                    this.output(0);
+                }
+
+            }; // end that
+            
+        } // end check on screen
+        
+}; // end init
+
+//  These are the calculator's publicly facing methods.
+
+//  Enter number is called when a user presses a digit, and takes that 
+//  digit as a param. If possible, enterNumber will pass that digit 
+//  to the inner calculator's update method.
 
     this.enterNumber = function(num){
-        that.update(num);
+        if( 'update' in that ){
+            that.update(num);
+        }
     };
-    
+
+//  Enter operator is called when a user presses an operator, and takes that 
+//  operator as a param. If possible, enterOperator will pass that operator 
+//  to the inner calculator's evaluate method.
+
     this.enterOperator = function(opp){
-        that.evaluate(opp);
+        if( 'evaluate' in that ){
+            that.evaluate(opp);
+        }
     };
-    
+ 
+//  Clear is called when a user presses the clear button, and takes 
+//  no params. If possible, clear will call the inner calculator's 
+//  reset method.
+  
     this.clear = function(){
-        that.reset();
+        if( 'reset' in that ){
+            that.reset();
+        }
     };
     
 }; // end calculator definition
@@ -139,9 +157,23 @@ var calculator = new function(){
 
 (function(){
     
+//  First, I will check if document.getElementsByClassName is natifley 
+//  defined, and define it myself if not.
+    
     checkGetClass();
     
+//  Second, run setup routine, in which page elements are bound to
+//  calculator methods.
+    
+    
+//  Start by calling the calcuator's init method, passing
+//  in an output element to be the display.
+
     calculator.init(document.getElementById('out'));
+    
+ 
+//  Move onto binding the numbers' click
+//  event to the calculator's update method.
 
     var numbers = document.getElementsByClassName('number');
     
@@ -150,8 +182,7 @@ var calculator = new function(){
 //      clicking a number will update the display and the temp variable
 
         numbers[i].onclick = function(){
-//            that.update(this.value); // that=calc this=number
-              calculator.enterNumber(this.value);
+              calculator.enterNumber(this.value); // this = a number btn
         }
     }
 
@@ -162,8 +193,7 @@ var calculator = new function(){
 //      clicking an operator will make the calculator perform an operation.
 
         operators[k].onclick = function(){
-//            that.evaluate(this.value); // that=calc this=opp
-              calculator.enterOperator(this.value);
+              calculator.enterOperator(this.value); // this = a function btn
         }
 
     }
@@ -171,7 +201,6 @@ var calculator = new function(){
 //  lastly, bind the clear function to the clear buttons' click event
 
     document.getElementById('clear').onclick = function(){
-//        that.reset(); // that=calc this=clear button
           calculator.clear();
     }
     
