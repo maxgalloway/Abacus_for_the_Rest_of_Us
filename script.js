9281c78c5b5d40661e5ad00b8fad522ecc48c1eb
MaGiC <maxvgc@gmail.com>
2013-01-30 00:41:33 -0600

    Put main files on agpl

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 //  The calculator pseudoClass represents a four-function calculator.
 //  The following both defines and constructs the singleton calculator object.
 
+//  This file is part of Abacus For The Rest Of Us
+//  Copyright (C) 2013 Max Galloway-Carson
+//    
+//  Abacus For The Rest Of Us is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//    
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Affero General Public License for more details.
+//    
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program. If not, see <http://www.gnu.org/licenses/>.
+//    
+//  email me at: maxvgc@gmail.com
+
 var calculator = (function () {
 
     'use strict';
