cb12d378252fa399b6da4d52fbbadce372721a96
MaGiC <maxvgc@gmail.com>
2013-02-03 15:13:46 -0600

    put agpl stuff at top of setup file

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,25 @@
 /**
  * This is where the setup for the calculator happens. It's all inside an
  * anonymous function, so the vars and the functions will go away at the end.
+ * 
+ * This file is part of Abacus For The Rest Of Us Copyright (C) 2013 Max
+ * Galloway-Carson
+ * 
+ * Abacus For The Rest Of Us is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http:*www.gnu.org/licenses/>.
+ * 
+ * email me at: maxvgc@gmail.com
+ * source code: https://github.com/maxvgc/abacus-for-the-rest-of-us
  */
 
 /*global calculator:false , localStorage:false*/
